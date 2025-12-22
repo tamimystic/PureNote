5d5c8c3fe126bc8c337a9089e88b769fbf26ec08
tamimystic <tamimhossainforcoding01@gmail.com>
2025-12-22 23:30:38 +0600

    change logger code

--- src/PureNote/logger.py
+++ src/PureNote/logger.py
@@ -8,16 +8,13 @@ log_dir = "./logs"
 log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOG_FORMAT,
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
 logger = logging.getLogger("PureNote")
-logger.setLevel(logging.INFO)
-
-formatter = logging.Formatter(LOG_FORMAT)
-
-file_handler = logging.FileHandler(log_filepath)
-file_handler.setFormatter(formatter)
-
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
